be065d452317a006143bed8c0b9cdb456ad5efbb
mumuxizi meng <2401681764@qq.com>
2013-05-09 15:35:44 +0800

    accor hotel
    change SelectAHotel.html, style.css,accor.js

--- accor_hotel/assets/js/accor.js
+++ accor_hotel/assets/js/accor.js
@@ -3,4 +3,10 @@ $(function(){
 
 		$('.accor_selecthotel_headselect_list').toggle();
 		});	
+		
+		$('.accor_selecthotel_headselect_list li').click(function(){
+			var accor_selecthotel_this=$('.accor_selecthotel_headselect_list li').eq($('.accor_selecthotel_headselect_list li').index(this)).html();
+			$(this).parent().parent().prev().children('.accor_selecthotel_headerselettitle').html(accor_selecthotel_this);
+			$(this).parent().parent().hide();
+			});
 });
